b1a3acef10622d3ea29e24af6471ac823ecc3398
Will D <wdemilio@gmail.com>
2016-04-11 15:16:57 +0000

    Hello secure world

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
+
+const crypto = require('crypto');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var assert = require('assert');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var https = require('https');
+var http = require('http');
+
 var app = express();
+
+var forceSSL = require('express-force-ssl');
+
+var httpsSettings = require('../https/settings.js');
+var fs = require('fs');
+
+
+var sslOptions = httpsSettings.options;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(forceSSL);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -56,5 +76,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+console.log(sslOptions);
+https.createServer(sslOptions, app).listen(httpsSettings.ports.https);
+http.createServer(app).listen(httpsSettings.ports.http);
 
 module.exports = app;
